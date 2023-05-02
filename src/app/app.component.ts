aa08d5f3c0de5a0a2c05d6ea5f35e86699fbbf71
Kaisoku <khuss063@gmail.com>
2023-05-02 11:31:06 +0200

    dev: standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RouterModule } from '@angular/router';
+
 @Component({
+  standalone: true,
+  imports: [WelcomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'standalone-components';
-}
+export class AppComponent {}
